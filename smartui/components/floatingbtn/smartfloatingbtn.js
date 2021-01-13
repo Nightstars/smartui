d49b2bbca2725ac094f92d52c80ebd7e477165c3
Nightstars <ChristZhangowner@gmail..com>
2021-01-13 13:35:12 +0800

    publish to npm

---
+++ smartui/components/floatingbtn/smartfloatingbtn.js
@@ -0,0 +1,30 @@
+// smartui/components/floatingbtn/floatingbtn.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    bg:{
+      type:String,
+      default:''
+    },
+    loadModal:{
+      type:Boolean,
+      default:false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ smartui/moudles/navigation/navigation.js
@@ -0,0 +1,37 @@
+/*
+* A weapp packag library of the navigation function.
+*
+* FILE:	navigation.js
+* VERSION:	1.0
+* AUTHOR:	Christ Chang <christzhangowner@gmail.com>
+*
+* NOTE: This version is not tested thoroughly!
+*
+* Copyright (c) 2021, Christ Chang
+* All rights reserved.
+*/
+
+//navigate to another page
+// 2021-01-04
+// by: Christ Chang
+// url: https://github.com/Nightstars/smartui
+const to=(pageurl)=>{
+  wx.navigateTo({
+    url: pageurl
+  })
+}
+
+//redirect to another page
+// 2021-01-04
+// by: Christ Chang
+// url: https://github.com/Nightstars/smartui
+const redirect=(pageurl)=>{
+  wx.redirectTo({
+    url: pageurl
+  })
+}
+
+module.exports={
+  to: to,
+  redirect: redirect
+}
